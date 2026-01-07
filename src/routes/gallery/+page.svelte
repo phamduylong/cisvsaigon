8d2f2ba29aeb2971e8e03c8baa6459ee036045d6
longph <longphamduy2002@gmail.com>
2026-01-08 01:00:47 +0200

    Gallery with real image

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,24 +1,30 @@
 <script>
+	import PocketBase from 'pocketbase';
+
+	const pb = new PocketBase('https://longph.pockethost.io/');
 	import { onMount } from 'svelte';
 
-	let randNr = Date.now().toString();
-	randNr = randNr[randNr.length - 1];
+
 
 	/**
-	 * @typedef {{ image: string }} Slide
+	 * @typedef {{ image: string, alt: string }} Slide
 	 */
 
 	/** @type {Slide[]} */
 	const slides = $state([]);
 
 	onMount(async () => {
-		const res = await fetch(`https://picsum.photos/v2/list?page=${randNr}`);
+				/**
+		 * @typedef {{ content: string, caption: string, collectionId: string, id: string }} ImageRecord
+		 */
 
-		/** @type {{ download_url: string }[]} */
-		const json = await res.json();
-
-		for (let i = 0; i < 30 && i < json.length; i++) {
-			slides.push({ image: json[i].download_url });
+		/** @type {ImageRecord[]} */
+		const records = await pb.collection('images').getFullList();
+		for(let i = 0; i < records.length; ++i) {
+			slides.push({
+				image: `https://longph.pockethost.io/api/files/${records[i].collectionId}/${records[i].id}/${records[i].content}`,
+				alt: records[i].caption
+			})
 		}
 	});
 </script>
@@ -29,48 +35,30 @@
 	<div class="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-4">
 		<!-- Column 1 -->
 		<div class="grid gap-4">
-			<img class="h-auto max-w-full rounded-base" src={slides[0]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[1]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[2]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[3]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[4]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[5]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[6]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[7]?.image} alt="" />
+			<img class="h-auto max-w-full rounded-base" src={slides[0]?.image} alt={slides[0]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[1]?.image} alt={slides[1]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[2]?.image} alt={slides[2]?.alt} />
 		</div>
 
 		<!-- Column 2 -->
 		<div class="grid gap-4">
-			<img class="h-auto max-w-full rounded-base" src={slides[8]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[9]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[10]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[11]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[12]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[13]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[14]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[15]?.image} alt="" />
+			<img class="h-auto max-w-full rounded-base" src={slides[3]?.image} alt={slides[3]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[4]?.image} alt={slides[4]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[5]?.image} alt={slides[5]?.alt} />
 		</div>
 
 		<!-- Column 3 -->
 		<div class="grid gap-4">
-			<img class="h-auto max-w-full rounded-base" src={slides[16]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[17]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[18]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[19]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[20]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[21]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[22]?.image} alt="" />
+			<img class="h-auto max-w-full rounded-base" src={slides[6]?.image} alt={slides[6]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[7]?.image} alt={slides[7]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[8]?.image} alt={slides[8]?.alt} />
 		</div>
 
 		<!-- Column 4 -->
 		<div class="grid gap-4">
-			<img class="h-auto max-w-full rounded-base" src={slides[23]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[24]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[25]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[26]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[27]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[28]?.image} alt="" />
-			<img class="h-auto max-w-full rounded-base" src={slides[29]?.image} alt="" />
+			<img class="h-auto max-w-full rounded-base" src={slides[9]?.image} alt={slides[9]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[10]?.image} alt={slides[10]?.alt} />
+			<img class="h-auto max-w-full rounded-base" src={slides[11]?.image} alt={slides[11]?.alt} />
 		</div>
 	</div>
 </div>
