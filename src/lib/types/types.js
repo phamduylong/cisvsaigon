265141981231ae69a306822c7626a59f4f041a04
longph <longphamduy2002@gmail.com>
2026-01-09 00:12:09 +0400

    Add typedef for ImageRecord

---
+++ src/lib/types/types.js
@@ -0,0 +1,13 @@
+/**
+ * Represents an image record with metadata.
+ *
+ * @typedef {Object} ImageRecord
+ * @property {string} id
+ * @property {string} collectionId
+ * @property {string} collectionName
+ * @property {string} content
+ * @property {string} caption
+ * @property {string} created
+ * @property {string} updated
+ */
+export {};
