543062f0e7076a34cf88650ebf6f3f64e117784a
longph <longphamduy2002@gmail.com>
2026-01-10 01:24:37 +0200

    Layout server to pass user related data on to the client

---
+++ src/routes/+layout.server.js
@@ -0,0 +1,12 @@
+export const load = ({ locals }) => {
+	// if we have an authenticated user, pass it over so the client side knows
+	if (locals.user) {
+		return {
+			user: locals.user
+		};
+	}
+
+	return {
+		user: undefined
+	};
+};
