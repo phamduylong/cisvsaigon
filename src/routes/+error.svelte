83b4910e93ab42fe31665d1700f7fd653459789e
longph <longphamduy2002@gmail.com>
2026-01-10 01:17:49 +0200

    Add generic error handler page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { page } from '$app/state';
+	const emotes = [
+		'＞﹏＜',
+		'(っ °Д °;)っ',
+		'(;´༎ຶД༎ຶ`)',
+		'⊙﹏⊙∥',
+		'(´。＿。｀)',
+		'(；′⌒`)',
+		'(。﹏。*)',
+		'o(TヘTo)',
+		'.·´¯`(>▂<)´¯`·.',
+		'(⊙x⊙;)',
+		'ಠ_ರೃ',
+		'o((⊙﹏⊙))o',
+		'.Σ(っ °Д °;)っ',
+		'_(:з)∠)_',
+		'<@_@>'
+	];
+</script>
+
+<svelte:head>
+	<title>An error has occurred D:</title>
+</svelte:head>
+
+<div class="p-10 select-none" role="main">
+	<h1 class="my-10 h1">
+		{emotes[Math.floor(Math.random() * emotes.length)]}
+		Something went wrong...
+	</h1>
+	<h1 class="my-10 h1">Error: {page.error?.message}</h1>
+</div>
