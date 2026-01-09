1572b9146b7027b040f620345aeede36918fa515
longph <longphamduy2002@gmail.com>
2026-01-10 01:22:34 +0200

    Use pocketbase from locals instead of creating a new instance

--- src/routes/gallery/+page.server.js
+++ src/routes/gallery/+page.server.js
@@ -1,14 +1,10 @@
-import PocketBase from 'pocketbase';
-
-const pb = new PocketBase('https://longph.pockethost.io/');
-
 /** @type {import('./$types').PageServerLoad} */
-export async function load() {
+export async function load({ locals }) {
 	/**
 	 * @typedef {import('$lib/types/types.js').ImageRecord} ImageRecord
 	 */
 	/** @type ImageRecord[] */
-	const images = await pb.collection('images').getFullList();
+	const images = await locals.pocketBase.collection('images').getFullList();
 	return {
 		images
 	};
