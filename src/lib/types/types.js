1ab212158c20b055f9d97c99507b01735785e389
longph <longphamduy2002@gmail.com>
2026-01-11 02:30:24 +0200

    Add user and blog post records type to jsdoc

--- src/lib/types/types.js
+++ src/lib/types/types.js
@@ -10,4 +10,34 @@
  * @property {string} created
  * @property {string} updated
  */
+
+/** * Represents user record
+ *
+ * @typedef {Object} UserRecord
+ * @property {string} collectionId
+ * @property {string} collectionName
+ * @property {string} created
+ * @property {string} email
+ * @property {boolean} emailVisibility
+ * @property {string} id
+ * @property {string} updated
+ * @property {string} username
+ * @property {boolean} verified
+ * @property {string} avatar
+ * @property {string} displayName
+ */
+
+/**
+ * Represents a blog post record with metadata.
+ *
+ * @typedef {Object} BlogPostRecord
+ * @property {string} id
+ * @property {string} collectionId
+ * @property {string} collectionName
+ * @property {string} content
+ * @property {string} title
+ * @property {string} created
+ * @property {string} updated
+ * @property {UserRecord} expand
+ */
 export {};
