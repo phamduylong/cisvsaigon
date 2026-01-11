55301d6a3a3515a10f8a25d6968e72bc2f291498
longph <longphamduy2002@gmail.com>
2026-01-11 16:13:41 +0200

    Option to delete blog posts

--- src/lib/components/BlogPostPreviewCard.svelte
+++ src/lib/components/BlogPostPreviewCard.svelte
@@ -2,104 +2,108 @@
 	import SquarePlus from '@lucide/svelte/icons/square-plus';
 	import Pen from '@lucide/svelte/icons/pen';
 	import Timer from '@lucide/svelte/icons/timer';
-	import { Avatar } from '@skeletonlabs/skeleton-svelte';
+	import Trash from '@lucide/svelte/icons/trash';
+	import XIcon from '@lucide/svelte/icons/x';
+	import { Avatar, Dialog, Portal } from '@skeletonlabs/skeleton-svelte';
+	import { processDateString, calculateAverageReadingTime } from '$lib/helper/stringFunctions';
 	import { PUBLIC_POCKETBASE_FILE_URL } from '$env/static/public';
 	/**
 	 * @typedef {import('$lib/types/types.js').BlogPostRecord} BlogPostRecord
 	 * @typedef {import('$lib/types/types.js').UserRecord} UserRecord
 	 */
-	/** @type {(BlogPostRecord & { expand: { author: UserRecord } })[]} */
-	let { isPlaceHolder = true, post = null } = $props();
-	/**
-	 * Process date string to human readable format
-	 * Shows day, month and year if the year is not the current year
-	 * @param date {Date}
-	 */
-	function processDateString(date) {
-		const day = date.getDate();
-		const month = date.getMonth();
-		const year = date.getFullYear();
-		let displayYear = false;
-		const newDate = new Date();
+	/** @type {{
+	 *   isPlaceHolder?: boolean,
+	 *   post?: (BlogPostRecord & { expand: { author: UserRecord } }) | null
+	 * }} */
+	let { isPlaceHolder = false, post = null } = $props();
 
-		if (
-			newDate.getFullYear() - year > 1 ||
-			(newDate.getFullYear() - year <= 1 &&
-				(newDate.getMonth() > month || (newDate.getMonth() === month && newDate.getDate() < day)))
-		) {
-			displayYear = true;
-		}
-		return `${date.toLocaleString('default', { month: 'short' })} ${date.getDate()} ${displayYear ? date.getFullYear() : ''}`;
-	}
+	const animation =
+		'transition transition-discrete opacity-0 translate-y-[100px] starting:data-[state=open]:opacity-0 starting:data-[state=open]:translate-y-[100px] data-[state=open]:opacity-100 data-[state=open]:translate-y-0';
 
-	/**
-	 * Calculate the average reading time for the blog post.
-	 * @param {string} postContent
-	 */
-	function calculateAverageReadingTime(postContent) {
-		if (!postContent) return 'Nothing to read';
-		let nrOfWords = postContent.split(' ').length;
-		const readingTimeInMinutes = nrOfWords / 250;
-		if (readingTimeInMinutes < 0.5) {
-			return 'Less than a minute read';
-		} else {
-			const roundedTime = Math.round(readingTimeInMinutes);
-			if (roundedTime === 1) {
-				return '1 minute read';
-			} else {
-				return `${roundedTime} minutes read`;
-			}
-		}
+	async function deletePost() {
+		await fetch(`/blog/${post?.id}`, {
+			method: "DELETE"
+		});
 	}
 </script>
 
 <div class="max-h-fit grid-cols-1 space-y-2 card preset-filled-surface-100-900 p-4">
 	{#if isPlaceHolder === true}
-		<div class="flex flex-col items-center justify-center card preset-filled-surface-200-800 p-4">
-			<a class="btn flex flex-col preset-filled-surface-200-800 p-4" href="/blog/new/" rel="">
+		<div class="flex flex-col items-center justify-center card p-4">
+			<a class="btn flex flex-col preset-filled-surface-200-800 hover:preset-tonal p-4" href="/blog/new/" rel="">
 				<SquarePlus />
 				<span>Add new post</span>
 			</a>
 		</div>
 	{:else}
-		<!-- Picture and name -->
-		<div class="flex w-fit items-center space-x-2">
-			<Avatar class="inline size-8">
-				<Avatar.Image
-					src={`${PUBLIC_POCKETBASE_FILE_URL}/${post.expand.author.collectionId}/${post.expand.author.id}/${post.expand.author.avatar}/`}
-					alt="Author's avatar"
-				/>
-				<Avatar.Fallback>LT</Avatar.Fallback>
-			</Avatar>
-			<span>{post.expand.author.displayName}</span>
+		<!-- Picture and name and delete icon -->
+		<div class="flex w-full items-center justify-between">
+			<div class="flex justify-center items-center space-x-2">
+				<Avatar class="inline size-8">
+					<Avatar.Image
+						src={`${PUBLIC_POCKETBASE_FILE_URL}/${post?.expand.author.collectionId}/${post?.expand.author.id}/${post?.expand.author.avatar}/`}
+						alt="Author's avatar"
+					/>
+					<Avatar.Fallback>LT</Avatar.Fallback>
+				</Avatar>
+				<span>{post?.expand.author.displayName}</span>
+			</div>
+			<!-- Confirm delete dialog -->
+			<Dialog>
+				<Dialog.Trigger><button><Trash class="card-hover btn hover:preset-tonal p-1 rounded-sm h-6 w-6" size={16} /></button></Dialog.Trigger>
+				<Portal>
+					<Dialog.Backdrop class="fixed inset-0 z-50 bg-surface-50-950/50" />
+					<Dialog.Positioner class="fixed inset-0 z-50 flex justify-center items-center p-4">
+						<Dialog.Content class="card bg-surface-100-900 w-full max-w-xl p-4 space-y-4 shadow-xl {animation}">
+							<header class="flex justify-between items-center">
+								<Dialog.Title class="text-lg font-bold">Confirm delete?</Dialog.Title>
+								<Dialog.CloseTrigger class="btn-icon hover:preset-tonal">
+									<XIcon class="size-4" />
+								</Dialog.CloseTrigger>
+							</header>
+							<Dialog.Description>
+								Are you sure you want to delete this blog post? This operation is permanent and cannot be reverted!
+							</Dialog.Description>
+							<footer class="flex justify-end gap-2">
+								<Dialog.CloseTrigger class="btn preset-tonal">Cancel</Dialog.CloseTrigger>
+								<Dialog.CloseTrigger><button type="button" class="btn preset-filled" onclick={async () => {
+									await deletePost();
+									window?.location?.reload()
+								}}>OK</button></Dialog.CloseTrigger>
+							</footer>
+						</Dialog.Content>
+					</Dialog.Positioner>
+				</Portal>
+			</Dialog>
+			
 		</div>
 
-		<a href="/blog/post/{post.id}"
+		<a href="/blog/{post?.id}"
 			><h3 class="my-2 h3 font-bold hover:text-primary-600 md:my-4">
-				{post.title}
+				{post?.title}
 			</h3></a
 		>
 
 		<div class="flex items-center justify-between">
 			<div
-				class="mx-1 flex items-center space-x-2 self-end text-sm md:mx-2"
-				title="Last edited on {new Date(post.updated).toDateString()} {new Date(
-					post.updated
+				class="flex items-center space-x-2 self-end text-sm"
+				title="Last edited on {new Date(post?.updated || '').toDateString()} {new Date(
+					post?.updated || ''
 				).toTimeString()}"
 			>
 				<Timer size={16} class="inline" /><i class="inline cursor-default"
-					>{calculateAverageReadingTime(post.content)}</i
+					>{calculateAverageReadingTime(post?.content)}</i
 				>
 			</div>
 			<!-- Written and edited info -->
 			<div
-				class="mx-1 flex items-center space-x-2 self-end text-sm md:mx-2"
-				title="Last edited on {new Date(post.updated).toDateString()} {new Date(
-					post.updated
+				class="flex items-center space-x-2 self-end text-sm"
+				title="Last edited on {new Date(post?.updated || '').toDateString()} {new Date(
+					post?.updated || ''
 				).toTimeString()}"
 			>
 				<Pen size={16} class="inline" /><i class="inline cursor-default"
-					>{processDateString(new Date(post.updated))}</i
+					>{processDateString(new Date(post?.updated || ''))}</i
 				>
 			</div>
 		</div>
