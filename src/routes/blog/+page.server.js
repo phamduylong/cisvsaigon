8314c72bb22689adf875171755c64a523cecea82
longph <longphamduy2002@gmail.com>
2026-01-11 02:30:51 +0200

    Load function for blog page

---
+++ src/routes/blog/+page.server.js
@@ -0,0 +1,16 @@
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals }) {
+	/**
+	 * @typedef {import('$lib/types/types.js').BlogPostRecord} BlogPostRecord
+	 * @typedef {import('$lib/types/types.js').UserRecord} UserRecord
+	 */
+	/** @type {(BlogPostRecord & { expand: { author: UserRecord } })[]} */
+	const posts = await locals.pocketBase.collection('posts').getFullList({
+		sort: '-created',
+		expand: 'author'
+	});
+
+	return {
+		posts
+	};
+}
