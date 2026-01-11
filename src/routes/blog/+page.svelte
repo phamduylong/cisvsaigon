5fd1c18befec027d324f294857dc864b28a2ffce
longph <longphamduy2002@gmail.com>
2026-01-11 02:31:03 +0200

    Blog page to display preview cards

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,22 @@
-<img
-	class="w-screen"
-	src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHJ6aTVyZzhsa2s1eWRyMGx3ZjI0aHk3aWpjNjdodzB0c3h3ZDV6ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1XgIXQEzBu6ZWappVu/giphy.gif"
-	alt="Building"
-/>
+<script>
+	import BlogPostPreviewCard from '$lib/components/BlogPostPreviewCard.svelte';
+	const { data } = $props();
+</script>
+
+<svelte:head>
+	<meta name="description" content="CISV Saigon's blog page" />
+	<title>CISV Saigon - Blog</title>
+</svelte:head>
+
+<div class="container mx-auto flex h-full flex-col items-center justify-center p-4 md:p-10">
+	<div class="container grid h-full grid-cols-1 items-start gap-y-5 md:w-5/6 xl:w-3/5">
+		<h1 class="my-8 text-center h1 font-serif text-5xl">Blog</h1>
+		{#each data.posts as post (post.id)}
+			<BlogPostPreviewCard {post} isPlaceHolder={false} />
+		{/each}
+
+		{#if data.user}
+			<BlogPostPreviewCard isPlaceHolder={true} />
+		{/if}
+	</div>
+</div>
