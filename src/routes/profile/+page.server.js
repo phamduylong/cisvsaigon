b2641f8cc813917fe8744235c166e10309e980e3
longph <longphamduy2002@gmail.com>
2026-01-15 22:46:45 +0200

    Route handler to account for updating avatar

---
+++ src/routes/profile/+page.server.js
@@ -0,0 +1,31 @@
+import { error } from '@sveltejs/kit';
+export async function load({ locals }) {
+	return {
+		user: locals.user
+	};
+}
+
+export const actions = {
+	// update image
+	updateImage: async ({ locals, request }) => {
+		const formData = await request.formData();
+
+		try {
+			await locals.pocketBase.collection('users').update(locals.user?.id, formData);
+		} catch (e) {
+			/** @typedef {{ code: number, message: string, data: Object }} AuthError */
+
+			const err = /** @type {AuthError} */ (e);
+			console.error(err);
+			error(err.code ?? 500, err.message ?? 'Unknown error occurred.');
+		}
+
+		if (locals.pocketBase.authStore.isValid) {
+			locals.user = locals.pocketBase.authStore.model;
+			return `${locals.user?.collectionId}/${locals.user?.id}/${locals.user?.avatar}`;
+		} else {
+			locals.user = null;
+			error(500, 'An error occurred when authenticating');
+		}
+	}
+};
