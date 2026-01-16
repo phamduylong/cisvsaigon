d562bbdd32f9a6f116d1ce5319e79f9d8b9d6524
longph <longphamduy2002@gmail.com>
2026-01-16 17:23:05 +0200

    Implement password change capability

--- src/routes/profile/+page.server.js
+++ src/routes/profile/+page.server.js
@@ -1,6 +1,6 @@
 export const prerender = false;
 
-import { error } from '@sveltejs/kit';
+import { redirect, error } from '@sveltejs/kit';
 export async function load({ locals }) {
 	return {
 		user: locals.user
@@ -29,5 +29,64 @@ export const actions = {
 			locals.user = null;
 			error(500, 'An error occurred when authenticating');
 		}
+	},
+
+	updateUser: async({ locals, request}) => {
+		const formData = Object.fromEntries(await request.formData());
+
+		const displayName = formData['displayName'];
+
+		if(!displayName) {
+			error(400, 'Display name cannot be empty');
+		}
+
+		const newPassword = formData['newPassword'];
+
+		const confirmPassword = formData['confirmPassword'];
+
+		if((newPassword || confirmPassword) && newPassword !== confirmPassword) {
+			error(400, 'Password fields do not match.');
+		}
+
+		const passwordChange = newPassword && confirmPassword && newPassword === confirmPassword
+
+		if(passwordChange) {
+			try {
+				await locals.pocketBase.collection('users').update(locals.user?.id, {
+					displayName,
+					password: newPassword,
+					confirmPassword
+				});
+			} catch (e) {
+				/** @typedef {{ code: number, message: string, data: Object }} AuthError */
+	
+				const err = /** @type {AuthError} */ (e);
+				console.error(err);
+				error(err.code ?? 500, err.message ?? 'Unknown error occurred.');
+			}
+	
+			await locals.pocketBase.authStore.clear();
+			locals.user = null;
+			redirect(303, '/login');
+		} else {
+			try {
+				await locals.pocketBase.collection('users').update(locals.user?.id, {
+					displayName
+				});
+			} catch (e) {
+				/** @typedef {{ code: number, message: string, data: Object }} AuthError */
+	
+				const err = /** @type {AuthError} */ (e);
+				console.error(err);
+				error(err.code ?? 500, err.message ?? 'Unknown error occurred.');
+			}
+			
+			if(locals.pocketBase.authStore.isValid) {
+				locals.user = locals.pocketBase.authStore.model;
+			}
+			redirect(303, '/');
+		}
+
+
 	}
 };
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -6,8 +6,18 @@
 	let { data } = $props();
 	let avatarSrc = $state(`${data.user?.collectionId}/${data.user?.id}/${data.user?.avatar}/`);
 
+	let newPassword = $state("");
+	let confirmPassword = $state("");
+	let displayName = $state(data.user?.displayName);
+
+	const passwordEntered = $derived(newPassword || confirmPassword);
+
+	const passwordDoNotMatch = $derived(passwordEntered && newPassword !== confirmPassword);
+
+	const displayNameChanged = $derived(displayName !== data.user?.displayName);
+
     /**
-     * 
+     * Returns a promise whether image upload succeeded
      * @param {{ files: File[]}} details
      */
 	function uploadAvatar(details) {
@@ -37,16 +47,39 @@
 			}
 		});
 	}
+
+	function validateAndSubmitForm() {
+		const form = document.getElementById('updateUserForm');
+
+		if(!passwordEntered && displayNameChanged) {
+			form.submit();
+			return;
+		}
+
+		if(passwordEntered && passwordDoNotMatch) {
+			toaster.error({
+				title: 'Error',
+				description: 'Password fields do not match.',
+			});
+			return;
+		}
+
+		// form data is good, let's submit.
+		form?.submit();
+	}
+
 </script>
 
 <div
 	class="container mx-auto flex w-screen place-content-center items-center justify-center p-10 md:mt-10"
 >
 	<form
+		id="updateUserForm"
+		action="?/updateUser"
 		method="POST"
 		class="flex w-md flex-col items-center justify-center space-y-5 card bg-surface-50-950 p-5 md:w-lg md:space-y-10 lg:w-xl lg:p-10"
 	>
-		<Avatar class="inline size-32 rounded-full!">
+		<Avatar class="inline size-32 rounded-full! border-4 border-surface-950-50">
 			<Avatar.Image
 				class="size-full object-cover"
 				src={`${PUBLIC_POCKETBASE_FILE_URL}/${avatarSrc}`}
@@ -84,12 +117,12 @@
 		</FileUpload>
 		<label class="label">
 			<span class="label-text flex">Display name</span>
-			<input class="input" type="text" name="displayName" value={data.user?.displayName} required />
+			<input class="input" type="text" name="displayName" value={displayName} required />
 		</label>
 
 		<label class="label">
 			<span class="label-text flex">New password</span>
-			<input class="input" type="password" name="newPassword" placeholder="New password" required />
+			<input class="input" type="password" name="newPassword" placeholder="New password" minlength="8" maxlength="20" required bind:value={newPassword}/>
 		</label>
 
 		<label class="label">
@@ -97,10 +130,15 @@
 			<input
 				class="input"
 				type="password"
-				name="newPassword"
+				name="confirmPassword"
 				placeholder="Confirm password"
+				minlength="8"
+				maxlength="20"
 				required
+				bind:value={confirmPassword}
 			/>
 		</label>
+
+		<button type="button" class="btn preset-filled" onclick={() => validateAndSubmitForm()}>Save</button>
 	</form>
 </div>
