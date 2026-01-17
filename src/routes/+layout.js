b4509e6b798ee6dc9ee414a8b0901dba5e20cd45
longph <longphamduy2002@gmail.com>
2026-01-17 11:53:44 +0200

    inject speed insights to layout page

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,8 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
- 
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 // Vercel analytics
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+injectSpeedInsights();
