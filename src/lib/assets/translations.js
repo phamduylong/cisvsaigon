0287db729b152f5e5e1f2de442b94a0450742772
longph <longphamduy2002@gmail.com>
2026-01-19 18:04:54 +0200

    Add example resource strings

---
+++ src/lib/assets/translations.js
@@ -0,0 +1,10 @@
+/** @type {Record<string, Record<string, string>>} */
+export default {
+    'en': {
+      "menu.login": "Login"
+    },
+    'vi': {
+      "menu.login": "Đăng nhập"
+    },
+  };
+  
