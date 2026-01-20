2dca82da381c4817c92b26657c695b11ae3c164f
longph <longphamduy2002@gmail.com>
2026-01-20 16:18:31 +0200

    Add more terminologies

--- src/lib/assets/translations.js
+++ src/lib/assets/translations.js
@@ -1,10 +1,146 @@
 /** @type {Record<string, Record<string, string>>} */
 export default {
-    'en': {
-      "menu.login": "Login"
-    },
-    'vi': {
-      "menu.login": "Đăng nhập"
-    },
-  };
-  
+	en: {
+		'menu.menu': 'Menu',
+		'menu.mode': 'Mode',
+		'menu.language': 'Language',
+		'menu.login': 'Login',
+		'menu.gallery': 'Gallery',
+		'menu.events': 'Events',
+		'menu.blog': 'Blog',
+		'menu.logout': 'Logout',
+    'menu.profile': 'Profile',
+
+		'overview.title': 'Overview',
+		'overview.intro': 'We are a group of CISV members based in Saigon, Vietnam.',
+		'overview.what_is_cisv': 'What is CISV?',
+		'overview.cisv_description':
+			'CISV (Children’s International Summer Villages) is a global peace-education organization founded internationally in 1950 by Dr. Doris Twitchell Allen with the idea that children can become ambassadors for a more peaceful world by learning across cultures. The first CISV Village programme was held in the U.S. in 1951.',
+		'overview.history': 'Our history',
+		'overview.history_description':
+			'Started as a small branch of CISV Vietnam from the mid-to-late 2010s, CISV Saigon has been striving to be a reliable camp organizer, holding camps for participants who previously took part in camps in Hanoi - where the CISV Vietnam chapter is based. We organized our first Mini-Camp in 2024, where we hosted 64 participants and 2 camps for kids of the Village age group. Since then, we have organized another Mini-Camp in the summer of 2025 and a New Year Party event at the beginning of 2026.',
+		'overview.activity_2025': 'One of our activities in 2025',
+		'overview.plans_2026': 'Plans for 2026',
+		'overview.plans_2026_description':
+			'For 2026, we plan to organize another Mini-Camp for kids of age group Village (for 10-11 years olds) and Youth Meeting (for 12-13 years olds). Exact time and location will be updated later, but expect it to happen during June - July.',
+
+		'auth.staff_only': 'Intended for Staffies only',
+		'auth.email': 'Email',
+		'auth.password': 'Password',
+		'auth.submit': 'Submit',
+
+		'common.show_more': 'Show more',
+		'common.show_less': 'Show less',
+
+		'event.confirm_delete': 'Confirm delete?',
+		'event.confirm_delete_message':
+			'Are you sure you want to delete this event? This operation is permanent and cannot be reverted!',
+		'common.ok': 'OK',
+		'common.cancel': 'Cancel',
+		'event.add_new': 'Add new event',
+		'event.title': 'Title',
+		'event.title_placeholder': 'Title of the event',
+		'event.start_date': 'Start date',
+		'event.end_date': 'End date',
+		'event.registration_link': 'Registration link',
+		'event.registration_link_placeholder': 'Link to the register form',
+		'event.min_age': 'Min. age',
+		'event.max_age': 'Max. age',
+		'event.no_age_limit': 'No age limit',
+		'event.short_description': 'Short description',
+		'event.short_description_placeholder': 'Short description of what is the event about?',
+		'event.submit': 'Submit',
+
+		'blog.confirm_delete': 'Confirm delete?',
+		'blog.confirm_delete_message':
+			'Are you sure you want to delete this blog post? This operation is permanent and cannot be reverted!',
+		'blog.add_new': 'Add new post',
+		'blog.editor': 'Editor',
+		'blog.preview': 'Preview',
+		'blog.title': 'Title',
+		'blog.title_placeholder': 'Title of the blog post',
+
+		'editor.insert_headings': 'Insert headings',
+		'editor.h1': 'Heading 1',
+		'editor.h2': 'Heading 2',
+		'editor.h3': 'Heading 3',
+		'editor.h4': 'Heading 4',
+		'editor.h5': 'Heading 5',
+		'editor.h6': 'Heading 6',
+
+		'common.save': 'Save',
+		'common.back': 'Back'
+	},
+
+	vi: {
+		'menu.menu': 'Menu',
+		'menu.mode': 'Giao diện',
+		'menu.language': 'Ngôn ngữ',
+		'menu.login': 'Đăng nhập',
+		'menu.gallery': 'Thư viện ảnh',
+		'menu.events': 'Sự kiện',
+		'menu.blog': 'Bài viết',
+		'menu.logout': 'Đăng xuất',
+    'menu.profile': 'Hồ sơ',
+
+		'overview.title': 'Giới Thiệu',
+		'overview.intro': 'Chúng mình là các thành viên CISV đang sinh sống và hoạt động tại Sài Gòn.',
+		'overview.what_is_cisv': 'Vậy CISV là gì?',
+		'overview.cisv_description':
+			'CISV (Children’s International Summer Villages) là một tổ chức giáo dục hòa bình toàn cầu, được Tiến sĩ Doris Twitchell Allen thành lập vào năm 1950. Bà tin rằng thông qua việc giao lưu và học hỏi giữa các nền văn hóa, trẻ em có thể trở thành những đại sứ cho một thế giới hòa bình hơn. Chương trình CISV Village đầu tiên đã được tổ chức tại Hoa Kỳ vào năm 1951.',
+		'overview.history': 'Hành trình của chúng mình',
+		'overview.history_description':
+			'Khởi đầu là một nhánh nhỏ của CISV Việt Nam từ giai đoạn cuối những năm 2010, CISV Sài Gòn luôn nỗ lực trở thành đơn vị tổ chức trại hè uy tín, mang đến sân chơi cho các thành viên từng tham gia các kỳ trại tại Hà Nội – nơi đặt trụ sở chính của CISV Việt Nam. Năm 2024, chúng mình đã tổ chức kỳ Mini-Camp đầu tiên, chào đón 64 người tham gia và 2 trại hè dành cho độ tuổi Village. Kể từ đó, chúng tôi tiếp tục tổ chức thêm một kỳ Mini-Camp vào hè 2025 và New Year Party vào đầu năm 2026.',
+		'overview.activity_2025': 'Hoạt động tiêu biểu năm 2025',
+		'overview.plans_2026': 'Kế hoạch cho 2026',
+		'overview.plans_2026_description':
+			'Trong năm 2026, chúng mình dự kiến tổ chức một Mini - Camp dành cho độ tuổi Village (từ 10-11 tuổi) và Youth Meeting (từ 12-13 tuổi). Thời gian và địa điểm cụ thể sẽ được cập nhật sau, dự kiến sẽ diễn ra vào khoảng tháng 6 - tháng 7.',
+
+		'auth.staff_only': 'Dành riêng cho Staffies',
+		'auth.email': 'Email',
+		'auth.password': 'Mật khẩu',
+		'auth.submit': 'Đăng nhập',
+
+		'common.show_more': 'Xem thêm',
+		'common.show_less': 'Thu gọn',
+
+		'event.confirm_delete': 'Xác nhận xóa?',
+		'event.confirm_delete_message':
+			'Bạn có chắc chắn muốn xóa sự kiện này không? Thao tác này là vĩnh viễn và không thể hoàn tác!',
+		'common.ok': 'OK',
+		'common.cancel': 'Hủy',
+		'event.add_new': 'Thêm sự kiện mới',
+		'event.title': 'Tiêu đề',
+		'event.title_placeholder': 'Tiêu đề của sự kiện',
+		'event.start_date': 'Ngày bắt đầu',
+		'event.end_date': 'Ngày kết thúc',
+		'event.registration_link': 'Link đăng ký',
+		'event.registration_link_placeholder': 'Link dẫn đến form đăng ký',
+		'event.min_age': 'Tuổi tối thiểu',
+		'event.max_age': 'Tuổi tối đa',
+		'event.no_age_limit': 'Không giới hạn độ tuổi',
+		'event.short_description': 'Mô tả ngắn',
+		'event.short_description_placeholder': 'Mô tả ngắn gọn nội dung của sự kiện này',
+		'event.submit': 'Đăng sự kiện / Xác nhận',
+
+		'blog.confirm_delete': 'Xác nhận xóa?',
+		'blog.confirm_delete_message':
+			'Bạn có chắc chắn muốn xóa bài viết này không? Thao tác này là vĩnh viễn và không thể hoàn tác!',
+		'blog.add_new': 'Thêm bài viết mới',
+		'blog.editor': 'Editor',
+		'blog.preview': 'Preview',
+		'blog.title': 'Tiêu đề',
+		'blog.title_placeholder': 'Tiêu đề của bài viết',
+
+		'editor.insert_headings': 'Chèn tiêu đề',
+		'editor.h1': 'Tiêu đề 1',
+		'editor.h2': 'Tiêu đề 2',
+		'editor.h3': 'Tiêu đề 3',
+		'editor.h4': 'Tiêu đề 4',
+		'editor.h5': 'Tiêu đề 5',
+		'editor.h6': 'Tiêu đề 6',
+
+		'common.save': 'Lưu',
+		'common.back': 'Quay lại'
+	}
+};
